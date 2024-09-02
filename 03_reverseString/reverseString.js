c5159a43a96fa4cb244e71d2f99bace8ed56bdb0
Oreus-F <fourniol.lucas@gmail.com>
2024-09-02 17:17:20 +0200

    Change it for much easier way

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,15 +1,5 @@
 const reverseString = function(string) {
-
-let lengthReverse = -string.length;
-let resultReverse = "";
-let x = 1;
-
-for (let i = -1; i >= lengthReverse; i--) {
-    resultReverse = resultReverse.padEnd(x, string.at(i));
-    x++;
-}
-
-return resultReverse;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
