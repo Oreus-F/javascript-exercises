cf725bec2bd0e96e3758becaffa95981acc722bb
Oreus-F <fourniol.lucas@gmail.com>
2024-09-02 17:14:46 +0200

    Complete the exercice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, multiplier) {
+    if (multiplier < 0) {
+        return "ERROR"
+    }
+    singleString = `${word}`
+    return fullString = singleString.repeat(multiplier)
 };
 
 // Do not edit below this line
