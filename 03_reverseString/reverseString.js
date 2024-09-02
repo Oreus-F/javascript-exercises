aaf4dfcc6d7fe243c89974c541027bfb241421f0
Oreus-F <fourniol.lucas@gmail.com>
2024-09-02 17:16:21 +0200

    Complete it the HARD WAY
    I was thinking of Array but i  didnt follow that instinct so i did it with more difficulty, with a loop and PadEnd into new variable.
    I tried, succed, but now i know it can be done so more easily

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+let lengthReverse = -string.length;
+let resultReverse = "";
+let x = 1;
+
+for (let i = -1; i >= lengthReverse; i--) {
+    resultReverse = resultReverse.padEnd(x, string.at(i));
+    x++;
+}
+
+return resultReverse;
 };
 
 // Do not edit below this line
