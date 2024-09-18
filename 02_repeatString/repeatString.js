ab4ec03fd446441559f8b632e75cf042c410a051
Oreus-F <fourniol.lucas@gmail.com>
2024-09-18 12:28:22 +0200

    Setup for loop to repeat string based on functions  arguments

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,23 @@
+/* 
 const repeatString = function(word, multiplier) {
     if (multiplier < 0) {
         return "ERROR"
     }
     singleString = `${word}`
     return fullString = singleString.repeat(multiplier)
-};
+}; 
+*/
+
+
+const repeatString = function(word, multiplier) {
+    let result = "";
+    if (multiplier < 0) {
+        return result = "ERROR"
+    } for (let i = 0; i < multiplier; i++) {
+        result += word;
+    }
+    return result
+}
 
 // Do not edit below this line
 module.exports = repeatString;
