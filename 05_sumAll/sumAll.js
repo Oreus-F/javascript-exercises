275c8f80b607dd699894ee052ceb0830ba23f2db
Oreus-F <fourniol.lucas@gmail.com>
2024-09-18 17:17:03 +0200

    Make Error Statement more readable.
    Initially the error statement was in a single argument. i put it  in several smaller part group by parameters. Also the Number.isInterger works with every thing wich is not a primary number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,13 @@
 const sumAll = function(num1, num2) {
-    if ((typeof num1 !== 'number' || num1 % 1 !== 0 || num1 < 0) 
-|| (typeof num2 !== 'number' || num2 % 1 !== 0 || num2 < 0)) {
-        return "ERROR";
-    } else {
-        let x = Math.min(num1, num2);
-        let y = Math.max(num1, num2);
-        let result = 0;
-        for (x; x <= y; x++){
-            result += x
-    }
-        return result
-    }
-};
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    let result = 0;
+    for (min; min <= max; min++){
+        result += min
+    } return result;
+    };
 
 
 
