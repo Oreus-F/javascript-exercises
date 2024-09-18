7bab54f0767e030b7e308a0960fa8c2ac10e651f
Oreus-F <fourniol.lucas@gmail.com>
2024-09-18 17:02:27 +0200

    if statement for errors and for loop to sum the numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((typeof num1 !== 'number' || num1 % 1 !== 0 || num1 < 0) 
+|| (typeof num2 !== 'number' || num2 % 1 !== 0 || num2 < 0)) {
+        console.log(typeof num1, typeof num2)
+        return "ERROR";
+    } else {
+        let x = Math.min(num1, num2);
+        let y = Math.max(num1, num2);
+        let result = 0;
+        for (x; x <= y; x++){
+            result += x
+    }
+        return result
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
