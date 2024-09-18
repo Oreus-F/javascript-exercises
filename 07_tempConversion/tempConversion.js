072826de8e0216318eed27686eee1c1acc4854dc
Oreus-F <fourniol.lucas@gmail.com>
2024-09-18 18:08:56 +0200

    minus 32 to the value and divide it by 1,8
    did the same for fahrenheit but i took the temperture multply it by 1,8 and add 32 to it.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32)/1,8;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * 1,8) + 32;
+  return temp;
 };
 
 // Do not edit below this line
